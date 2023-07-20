358c61449608691acffc680317d1844a998235f0
czar991 <nevertheless997@gmail.com>
2023-07-20 19:18:54 +0300

    fixed

--- src/js/modules/mobile-nav.js
+++ src/js/modules/mobile-nav.js
@@ -8,15 +8,19 @@ function mobileNav() {
   const navBtn = document.querySelector('.mobile-nav-btn');
   const nav = document.querySelector('.header__nav');
   const menuIcon = document.querySelector('.nav-icon');
+  const navItems = document.querySelectorAll('#nav-link');
 
   navBtn.onclick = function () {
     nav.classList.toggle('header__nav--open');
     menuIcon.classList.toggle('nav-icon--active');
     document.body.classList.toggle('no-scroll');
-    if (header.classList.contains('sticky')) {
-      header.classList.toggle('sticky');
-    }
   };
+  navItems.forEach((item) => {
+    item.addEventListener('click', function () {
+      nav.classList.remove('header__nav--open');
+      menuIcon.classList.remove('nav-icon--active');
+      document.body.classList.remove('no-scroll');
+    });
+  });
 }
-
 export default mobileNav;
